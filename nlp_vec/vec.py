36bca81a29a5af1f345c5bda5a47ee3f6c563267
ogawan <nozomiogawa@gmail.com>
2021-09-25 12:52:10 +0000

    updated vec

--- nlp_vec/vec.py
+++ nlp_vec/vec.py
@@ -1,3 +1,19 @@
+class Vectorize_GloVe:
+    
+    def __init__(self):  
+        word2vec = {}
+        word_embedding = {}
+        index_dic = {}
+        
+        # directory to the pretrain weights downloaded from https://nlp.stanford.edu/projects/glove/
+        with open('word//glove.6B/glove.6B.50d.txt') as f:
+            print(f)
+            break
+
+
+    
+
+
 def word2vect(sentences):
     return sentences[0]
 
