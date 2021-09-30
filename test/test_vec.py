e63ce53d0f72ba91a48102ddbf558b3ab355ecd1
ogawan <nozomiogawa@gmail.com>
2021-09-30 11:19:48 +0000

    change path

--- test/test_vec.py
+++ test/test_vec.py
@@ -1,6 +1,6 @@
+import os 
 import unittest
 import numpy as np
-from pathlib import Path
 from nlp_vec import vec
 
 class Test_nlp(unittest.TestCase):
@@ -12,12 +12,12 @@ class Test_nlp(unittest.TestCase):
         self.assertEqual(vec.word(), "hello") 
         
     def test_Vectorize_GloVe(self):
-        path = Path().cwd() / "test" / "glove_test_data.npy"
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = dir_path + "/glove_test_data.npy"
         glove_vectorizer = vec.Vectorize_GloVe()
         vecs = glove_vectorizer.fit_transform(["This is an apple", "I am orange boy with a pinapple", "Fat elephant"])
         
         self.assertEqual((vecs == np.load(path)).all(), True) 
-        
         
 if __name__ == '__main__':
     unittest.main()
