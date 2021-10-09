9240a376552a4f25a073cce40d15d9b4d42fe03b
ogawan <nozomiogawa@gmail.com>
2021-10-09 10:47:54 +0000

    add utils

---
+++ nlp_vec/utils/utils.py
@@ -0,0 +1,3 @@
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
--- test/test_vec.py
+++ test/test_vec.py
@@ -2,6 +2,7 @@ import os
 import unittest
 import numpy as np
 from nlp_vec import vec
+from nlp_vet.utils import utils
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,6 +20,12 @@ class Test_nlp(unittest.TestCase):
         word2vec_vectorizer = vec.Vectorize_word2vec()
         vecs =  word2vec_vectorizer.fit_transform(["This is an apple", "I am orange boy with a pineapple", "Fat elephant", "organic chemistry", "Fuji mountain", "Megane TRPV1", "FGHSRHG$%&YEHDRT"])
         self.assertEqual((vecs == np.load(path)).all(), True) 
+    
+    def test_utils(self):
+        x = np.array([0.3, -1, 0.4, 0.3])
+        
+        self.assertEqual(utils(x), np.array([0.29605921,0.08068555,0.32719603, 0.29605921]))
+                    
         
 if __name__ == '__main__':
     unittest.main()
