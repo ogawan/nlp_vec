10ae0f406b5e28d61ce787302a7a2463348992de
ogawan <nozomiogawa@gmail.com>
2021-10-09 11:48:51 +0000

    passed test

--- test/test_vec.py
+++ test/test_vec.py
@@ -2,11 +2,15 @@ import os
 import unittest
 import numpy as np
 from nlp_vec import vec
-from nlp_vet.utils import utils
+from nlp_vec.utils import utils
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 class Test_nlp(unittest.TestCase):
+    
+    def test_utils(self):
+        x = np.array([0.3, -1, 0.4, 0.3])
+        self.assertEqual((utils.softmax(x).round(2)  == np.array([0.29605921,0.08068555,0.32719603, 0.29605921]).round(2) ).all(), True)
         
     def test_Vectorize_GloVe(self):
         path = dir_path + "/glove_test_data.npy"
@@ -20,12 +24,7 @@ class Test_nlp(unittest.TestCase):
         word2vec_vectorizer = vec.Vectorize_word2vec()
         vecs =  word2vec_vectorizer.fit_transform(["This is an apple", "I am orange boy with a pineapple", "Fat elephant", "organic chemistry", "Fuji mountain", "Megane TRPV1", "FGHSRHG$%&YEHDRT"])
         self.assertEqual((vecs == np.load(path)).all(), True) 
-    
-    def test_utils(self):
-        x = np.array([0.3, -1, 0.4, 0.3])
-        
-        self.assertEqual(utils(x), np.array([0.29605921,0.08068555,0.32719603, 0.29605921]))
-                    
+
         
 if __name__ == '__main__':
     unittest.main()
